338105b0e9b9b746df1fd14d2d9a81876223b30d
Avi Goel <avi.19b101025@abes.ac.in>
2022-03-04 22:28:39 +0530

    Update FirstNegativeNumInWindowOfSizeK.py

--- SlidingWindow/FirstNegativeNumInWindowOfSizeK.py
+++ SlidingWindow/FirstNegativeNumInWindowOfSizeK.py
@@ -23,6 +23,28 @@ Output:- -8 0 -6 -6
 Python Implementation:-
 """
 
+n = int(input())
+lst = list(map(int,input().split()))
+k = int(input())
+negative = []
+ws = we = 0
+ans = []
+while we < N:
+  if A[we] < 0:
+    negative.append(A[we])
+  if we - ws + 1 > K:
+    if len(negative) != 0 and negative[0] == A[ws]:
+      negative.pop(0)
+    ws += 1
+  if we - ws + 1 == K:
+    if len(negative) != 0:
+      ans.append(negative[0])
+    else:
+      ans.append(0)
+  we += 1
+print(ans)
+  
+
 
 
 
