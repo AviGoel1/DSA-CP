7e6019799733aeb0673aa8c8d3f599370f2e8d8c
Avi Goel <avi.19b101025@abes.ac.in>
2022-03-08 00:07:17 +0530

    Create FirstAndLastOccurenceOfElement.py
    Program to find the first and last occurrence of an element in an sorted array

---
+++ BinarySearch/FirstAndLastOccurenceOfElement.py
@@ -0,0 +1,33 @@
+def last(arr,l,h,x):
+  """ Here we are checking if element exists in upper half, if it is already found at an index. """
+  res = -1
+  while l <= h:
+    m = l + (h-l)//2
+    if arr[m] == x:
+      res = m
+      l = m+1
+    elif arr[m] <= x:
+      l = m + 1
+    else:
+      h = m - 1
+  return res
+    
+def first(self,arr,l,h,x):
+  """ Here we are checking if element exists in lower half, if it is already found at an index. """
+  res = -1
+  while l <= h:
+    m = l + (h-l)//2
+    if arr[m] == x:
+      res = m
+      h = m-1
+    elif arr[m] >= x:
+      h = m - 1
+    else:
+      l = m + 1
+  return res
+
+n = int(input())
+arr = list(map(int,input().split()))
+x = int(input())
+print(first(arr,0,n-1,x))
+print(last(arr,0,n-1,x))
